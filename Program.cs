60659de5c033f1cb78a9f1a11765feaf19bd63d6
Francesco Tappi <france.tappi@gmail.com>
2022-08-04 11:52:26 +0200

    withdrawal and deposit methods + transactions test

--- Program.cs
+++ Program.cs
@@ -3,3 +3,8 @@ using Classes;
 
 var account = new BankAccount("Francesco", 1000);
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
